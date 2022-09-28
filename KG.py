076e598cdea12cfd472220613f30224492efd273
lwb69 <67140264+lwb69@users.noreply.github.com>
2022-09-28 15:58:16 +0800

    Add files via upload

---
+++ KG.py
@@ -0,0 +1,64 @@
+from neo4j import GraphDatabase
+
+#neo4j
+driver = GraphDatabase.driver("bolt://localhost:7687",auth=("neo4j","mm430422"))
+session = driver.session()
+nametypes=['中药名称', '中药分类名称', '其他治法名', '方剂名称', '方剂分类名称', '皮肤病名称', '证候名称', '证候分类名称', '辨证法名称']
+def GetRelationPathsSingle(entity):
+    '''根据实体名，得到所有1跳关系路径（实体属性也视为关系）,实体名属性在各个实体中不同'''
+    PathsSingle={}
+    for nametype in nametypes:
+        cql_1 = "match (a)-[r1]-() where a.{}=$name return DISTINCT type(r1)".format(nametype)
+        cql_2="match (a)where a.{}=$name return properties(a)".format(nametype)
+        rpaths1 = []
+        props_list=[]
+        res = session.run(cql_1, name=entity)#一个多个record组成的集合
+        props=session.run(cql_2, name=entity)
+        for record1 in res:#每个record是一个key value的有序序列
+            rpaths1.append(record1['type(r1)'])
+        for record2 in props:
+            for prop in record2['properties(a)'].keys():
+                props_list.append(prop)
+        if len(rpaths1)==0 and len(props_list)==0:
+            continue
+        PathsSingle[nametype]={'relation':rpaths1,'propertise':props_list}
+    return PathsSingle
+
+
+def GetRelationPaths(entity):
+    '''根据实体名，得到所有2跳关系路径，用于问题和关系路径的匹配'''
+    Paths2hop={}
+    for nametype in nametypes:
+        cql_1 = "match (a)-[r1]-()-[r2]-() where a.{}=$name return DISTINCT type(r1),type(r2)".format(nametype)
+        cql_2 = "match (a)-[r1]-(b)where a.{}=$name return type(r1),properties(b)".format(nametype)
+        rpaths1 = []
+        path2props= []
+        res = session.run(cql_1,name=entity)
+        resprop=session.run(cql_2,name=entity)
+        for record1 in res:
+            rpaths1.append([record1['type(r1)'],record1['type(r2)']])
+        for record2 in resprop:
+            for prop in record2['properties(b)'].keys():
+                path2props.append((tuple([record2['type(r1)'],prop])))
+        path2props_set=[list(item) for item in set(path2props)]
+        if len(rpaths1)==0 and len(path2props_set)==0:
+            continue
+        Paths2hop[nametype] = {'relation2hop': rpaths1, 'rel-prop': path2props_set}
+    return Paths2hop
+
+
+def GetRelationNum(entity):
+    '''根据实体名，得到与之相连的关系数量，代表实体在知识库中的流行度'''
+    relcounts=0
+    for nametype in nametypes:
+        cql1= "match p=(a)-[r1]-() where a.{}=$name return count(p)".format(nametype)
+        res = session.run(cql1,name=entity)
+        ans = 0
+        for record in res:
+            ans = record.values()[0]
+        relcounts+=ans
+    return relcounts
+if __name__ == '__main__':
+
+    a=GetRelationNum('天疤疮')
+    print(a)
---
+++ path_feature.py
@@ -0,0 +1,40 @@
+import pickle
+
+from utils import *
+
+def GetTuplefeature(corpus):
+    '''
+    为path生成相关特征
+    :param corpus:
+    :return: corpus
+    '''
+    num=0
+    for i in range(len(corpus)):
+        dic = corpus[i]
+        question = dic['question']
+        gold_tuple = dic['gold_tuple']
+        if len(gold_tuple) >= 4:
+            # print(gold_tuple)
+            continue
+        candidate_tuples = dic['candidate_tuples']
+        if gold_tuple in candidate_tuples:
+            num+=1
+        for candidata_tuple in candidate_tuples:
+            candidate_path=''.join(candidata_tuple)
+            features=features_from_two_sequences(candidate_path,question)
+            candidate_tuples[candidata_tuple]=candidate_tuples[candidata_tuple]+features
+            # if gold_tuple ==candidata_tuple:
+            #     print(candidate_tuples[candidata_tuple])
+
+        dic['candidate_tuples']=candidate_tuples
+    print(num)
+        # print(gold_tuple)
+    return corpus
+if __name__ == '__main__':
+    inputpaths=['../data/candidate_tuples_train.pkl','../data/candidate_tuples_valid.pkl']
+    outputpaths=['../data/candidate_tuples_feature_train.pkl','../data/candidate_tuples_feature_valid.pkl']
+
+    for inputpath,outputpath in zip(inputpaths,outputpaths):
+        corpus= pickle.load(open(inputpath, 'rb'))
+        corpus=GetTuplefeature(corpus)
+        pickle.dump(corpus, open(outputpath, 'wb'))
